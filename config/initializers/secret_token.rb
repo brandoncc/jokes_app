4f56c1eef71609f6a0d428a8c1c78bbe478e8634
Brandon Conway <brandoncc@gmail.com>
2014-03-07 10:28:54 -0800

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JokesApp::Application.config.secret_key_base = '800b29e9ae3da66443bb5f227f16c45fa579ff5a8316fe5df0b345bd768a2f0ba6eed4e20ae94a9546622aa6bf3aac3ecc8b7c93ed8c3ac45f92ec631b977085'
